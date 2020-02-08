d90b5a9b1148871104390e435daa45202f85187a
Brett Miller <brett@localhost.localdomain>
2020-02-08 20:30:26 +1300

    Fix compare ALL parameters & replace unreliable SchImplementation method with  old PCB board iterator method

--- Project/CheckPrjCompFPMappings.pas
+++ Project/CheckPrjCompFPMappings.pas
@@ -12,6 +12,8 @@ B. Miller
 21/08/2018  v0.1  intial
 15/12/2018  v0.2  added message panel stuff
 07/02/2020  v0.3  report cleanup: report differences of model names, libraries & parameters
+08/02/2020  v0.31 Fix check all parameters & go back to old PCB iterator method for library checks.
+                  CompImpl.DM_DatafileLibraryIdentifier(0) does not work for all comps.
 
 
 ..............................................................................}
@@ -101,7 +103,7 @@ begin
     I := 0;
     bFound := false;
 
-    while (not bFound) and (I < (Comp.DM_ParameterCount - 1)) do
+    while (not bFound) and (I < (Comp.DM_ParameterCount) ) do
     begin
         Param := Comp.DM_Parameters(I);
         if SameString(Param.DM_Name, PName, False) then
@@ -113,6 +115,35 @@ begin
     end;
 end;
 
+function FindPCBComponent(Comp : IComponent) : IPCB_Component;
+var
+    PCBComp        : IPCB_Component;
+    Iterator       : IPCB_BoardIterator;
+    bFound         : boolean;
+begin
+    Result := nil;
+    If Board = Nil Then Exit;
+
+    Iterator := Board.BoardIterator_Create;
+    Iterator.AddFilter_ObjectSet(MkSet(eComponentObject));
+    Iterator.AddFilter_IPCB_LayerSet(MkSet(eTopLayer,eBottomLayer));
+    Iterator.AddFilter_Method(eProcessAll);
+
+    bFound := false;
+
+    PCBComp := Iterator.FirstPCBObject;
+
+    while (not bFound) and (PCBComp <> Nil) Do
+    begin
+        if Comp.DM_UniqueId = PCBComp.SourceUniqueID then
+        begin
+            Result := PCBComp;
+            bFound := true;
+        end;
+        PCBComp := Iterator.NextPCBObject;
+    End;
+    Board.BoardIterator_Destroy(Iterator);
+end;
 
 Procedure ReportCompMappings;
 var
@@ -120,11 +151,14 @@ var
     PrimDoc        : IDocument;
 
     Comp           : IComponent;
-    TargetComp     : iComponent;
+    TargetComp     : IComponent;
+    PCBComp        : IPCB_Component;
     CompImpl       : IComponentImplementation;
     TargetImpl     : IComponentImplementation;
-    Param      : IParameter;
-    FPParam    : IParameter;
+    SchImplLib     : WideString;
+    PcbFPLib       : WideString;
+    Param          : IParameter;
+    FPParam        : IParameter;
     CurrentSch     : ISch_Document;
     I, J, K, L, M  : Integer;
     TotLLinkCount  : Integer;            // Total mismatched library link count
@@ -133,6 +167,7 @@ var
     FLinkCount     : Integer;
     SMess          : WideString;
     bFound         : boolean;
+    bSkip          : boolean;
 
 Begin
     WS  := GetWorkspace;
@@ -146,7 +181,15 @@ Begin
     IntLibMan := IntegratedLibraryManager;
     If IntLibMan = Nil Then Exit;
 
+    Board := PCBServer.GetCurrentPCBBoard;
     PrimDoc := Prj.DM_PrimaryImplementationDocument;
+    if PrimDoc = nil then exit;
+    If Board = nil then
+        Board := PcbServer.GetPcbBoardByPath(PrimDoc.DM_FullPath);
+    if Board = Nil then
+        Board := PcbServer.LoadPcbBoardByPath(PrimDoc.DM_FullPath);
+    If Board = Nil Then Exit;
+
 // for the DMObject Componentmapping interface
     PrjMapping := Prj.DM_ComponentMappings(PrimDoc.DM_FullPath);   //Board.FileName);
 
@@ -219,14 +262,16 @@ Begin
         for I := 0 to (PrjMapping.DM_UnmatchedTargetComponentCount - 1) do
         begin
             Comp := PrjMapping.DM_UnmatchedTargetComponent(I);
+            PCBComp := FindPCBComponent(Comp);
+
             PrjReport.Add('Unmatched in PcbDoc : ' + IntToStr(I + 1) + ' ' + Comp.DM_FullLogicalDesignatorForDisplay
                           + '  ' + Comp.DM_FootPrint + '  ' + Comp.DM_LibraryReference);
             MMObjAddr := Comp.DM_ObjectAdress;
             MMessage := 'UnMatched PCB Component  : '+ IntToStr(I) + ' | ' + Comp.DM_FullLogicalDesignatorForDisplay + + ' | ' + Comp.DM_FootPrint;
 
-// this is not working..
-            if Comp.DM_ImplementationCount > 0 then
-                MMessage := MMessage + ' | ' + FindParameterValue(Comp , 'Library Name');
+// need find method that does not use uniqueID as unmatched PCb comps might not have them.
+            if PCBComp <> nil then
+                MMessage := MMessage + ' | ' + PCBComp.SourceFootprintLibrary;   //  FindParameterValue(Comp , 'Library Name');
 
             AddMessage(MMPanel, '[Info]', MMessage , MSource, PrimDoc.DM_FileName, 'WorkspaceManager:View', 'CrossProbeConnective=' + IntToStr(MMObjAddr), IMG_Component);
         end;
@@ -244,7 +289,6 @@ Begin
             LLinkCount := 0; FLinkCount := 0;
 
             PrjReport.Add('');
-            PrjReport.Add('');
             PrjReport.Add('Sheet : ' + IntToStr(I+1) + '  ' + Doc.DM_FileName + ' ' + IntToStr(Doc.DM_ChannelIndex) );
             PrjReport.Add('');
             for J := 0 to Doc.DM_ComponentCount - 1 Do
@@ -256,26 +300,41 @@ Begin
                 PrjReport.Add(' Comp FootPrint   : ' + Comp.DM_FootPrint);
                 PrjReport.Add(' Current FP Model : ' + Comp.DM_CurrentImplementation(cDocKind_PcbLib).DM_ModelName + '  ModelType :' + Comp.DM_CurrentImplementation(cDocKind_PcbLib).DM_ModelType);
 
+             // old way to get PCB footprint
+                PCBComp := FindPCBComponent(Comp);
+
 // report the project mapping source sch to target pcb
 // Matched Source & Target have the same index
 
-
                 for K := 0 to (PrjMapping.DM_MatchedComponentCount - 1) do
                 begin
-                    if PrjMapping.DM_MatchedSourceComponent(K).DM_UniqueId = Comp.DM_UniqueId then
+                    if (PrjMapping.DM_MatchedSourceComponent(K).DM_UniqueId = Comp.DM_UniqueId) then
                     begin
+
                         TargetComp := PrjMapping.DM_MatchedTargetComponent(K);
-                        CompImpl := Comp.DM_CurrentImplementation(cDocKind_PcbLib);
+                        CompImpl   := Comp.DM_CurrentImplementation(cDocKind_PcbLib);
+                    // Is one Impl in PCB, & it has no model files or datalinks.
+                        TargetComp.DM_ImplementationCount ;
                         TargetImpl := TargetComp.DM_CurrentImplementation(cDocKind_PcbLib);
-                    //    PrjReport.Add(' Mapping Target : ' + IntToStr(K) + ' | Des: ' + TargetComp.DM_FullLogicalDesignatorForDisplay);
-                        TargetImpl.DM_DatafileCount ;
+
+// the below method does not always return the SchImp lib path ?????
                         CompImpl.DM_DatafileCount;
+                        SchImplLib := CompImpl.DM_DatafileLibraryIdentifier(0);
+                        CompImpl.DM_DatafileEntity(0);
+                        CompImpl.DM_DatafileLocation(0);
+
+                        PcbFPLib   := TargetComp.DM_SourceLibraryName ;   // TargetImpl.DM_DatafileLibraryIdentifier(0);
+
+                    //    PrjReport.Add(' Mapping Target : ' + IntToStr(K) + ' | Des: ' + TargetComp.DM_FullLogicalDesignatorForDisplay);
 
                         if (CompImpl.DM_ModelName = TargetImpl.DM_ModelName) then
                         begin
-                            if Comp.DM_SourceLibraryName <> TargetComp.DM_SourceLibraryName then
+                   // this does not work
+                   //         if trim(SchImplLib) <> trim(PcbFPLib) then
+                            if (PCBComp <> nil) then
+                            if PCBComp.SourceFootprintLibrary <> PCBComp.SourceComponentLibrary then
                             begin
-                                PrjReport.Add('*** mismatched FP model libs : ' + Comp.DM_SourceLibraryName + ' <> ' + TargetComp.DM_SourceLibraryName + '  ***');
+                                PrjReport.Add('*** mismatched FP model libs : ' + PCBComp.SourceComponentLibrary + ' <> ' + PCBComp.SourceFootprintLibrary + '  ***');
                                 inc(LLinkCount);
                             end;
 
@@ -288,7 +347,9 @@ Begin
                                 Param := Comp.DM_Parameters(L);
                                 M := 0;
                                 bFound := false;
-                                while (not bFound) and (M < TargetComp.DM_ParameterCount) do
+                                bSkip := CheckIgnoreParameter(Param.DM_Name);
+
+                                while (not bSkip) and (not bFound) and (M < TargetComp.DM_ParameterCount) do
                                 begin
                                     FPParam := TargetComp.DM_Parameters(M);
                                     Inc(M);
@@ -301,9 +362,8 @@ Begin
                                     Param.DM_Kind;
                                     Param.DM_ConfigurationName;
                                 end;
-                                if not bFound then
-                                    if not CheckIgnoreParameter(Param.DM_Name) then
-                                        PrjReport.Add('*** missing FP parameter ' + PadRight(IntToStr(L + 1), 3) + '  ' + Param.DM_Name + ' | ' + Param.DM_Value + ' ***');
+                                if (not bSkip) and (not bFound) then
+                                    PrjReport.Add('*** missing FP parameter ' + PadRight(IntToStr(L + 1), 3) + '  ' + Param.DM_Name + ' | ' + Param.DM_Value + ' ***');
                             end;
 
                         end
@@ -311,10 +371,12 @@ Begin
                             PrjReport.Add('***  non-matching Source-Target FP ModelNames  ***');
                             inc(FLinkCount);
                         end;
-
-
                     end;
+
                 end;  // k prjmapping
+
+                if PCBComp = nil then
+                    PrjReport.Add('***  strange ? missing PCB Component  ***');
 
                 PrjReport.Add('');
             end;  // j dm_components
@@ -381,44 +443,4 @@ End;
 // Function DM_MatchedComponentCount: Integer;  Returns the number of matched components.
 // Function DM_MatchedSourceComponent  (Index : Integer) : IComponent;  Returns the indexed matched source component (that has been matched with a target component). Use the DM_MatchedSourceComponentCount function.
 // Function DM_MatchedTargetComponent  (Index : Integer) : IComponent;  Returns the indexed matched source component (that has been matched with a target component). Use the DM_MatchedTargetComponentCount function.
-
-{
-var
-    PCBComp        : IPCB_Component;
-    Iterator       : IPCB_BoardIterator;
-
-                for K := 0 to (Comp.DM_ImplementationCount - 1) do
-                begin
-                    CompImpl := Comp.DM_Implementations(K);
-                    if (CompImpl.DM_ModelType = cDocKind_PcbLib) and CompImpl.DM_IsCurrent then
-                    begin
-                    //  GetMatchCompInPCB
-                       Iterator := Board.BoardIterator_Create;
-                       Iterator.AddFilter_ObjectSet(MkSet(eComponentObject));
-                       Iterator.AddFilter_IPCB_LayerSet(MkSet(eTopLayer,eBottomLayer));
-                       Iterator.AddFilter_Method(eProcessAll);
-
-                       PCBComp := Iterator.FirstPCBObject;
-
-                       While (PCBComp <> Nil) Do
-                       Begin
-                           if  Comp.DM_UniqueId = PCBComp.SourceUniqueID then
-                           begin
-                               PrjReport.Add('found matching component');
-                               if CompImpl.DM_ModelName = PCBComp.Pattern then
-                                   PrjReport.Add('found matching FP ModelName')
-                               else
-                                   PrjReport.Add('No matching FP ModelName found!');
-                               if PCBComp.SourceFootprintLibrary <> PCBComp.SourceComponentLibrary then
-                                   PrjReport.Add(' ----------- Footprint Warning check  : '  + PCBComp.Pattern + '  -----------');
-                           end;
-                           PCBComp := Iterator.NextPCBObject;
-                       End;
-
-                       Board.BoardIterator_Destroy(Iterator);
-
-                    end;
-
-                end;  // k implementations
-}
 
