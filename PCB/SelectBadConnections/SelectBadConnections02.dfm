dd193dabcbecba419306138e294d77d44584326a
Brett Miller <brett@localhost.localdomain>
2020-06-13 12:19:10 +1200

    init commit SelectBadConnection using diff form & full? layer support (tests okay AD17)

---
+++ PCB/SelectBadConnections/SelectBadConnections02.dfm
@@ -0,0 +1,110 @@
+object FormSelectBadConnections: TFormSelectBadConnections
+  Left = 0
+  Top = 0
+  Caption = 'Select Bad Connections'
+  ClientHeight = 153
+  ClientWidth = 236
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormSelectBadConnectionsCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 24
+    Top = 8
+    Width = 57
+    Height = 13
+    Caption = 'Tolerance:'
+  end
+  object Label2: TLabel
+    Left = 24
+    Top = 56
+    Width = 72
+    Height = 13
+    Caption = 'Active Layers:'
+  end
+  object EditTolerance: TEdit
+    Left = 16
+    Top = 24
+    Width = 72
+    Height = 24
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -13
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 0
+    Text = '0.1'
+    OnChange = EditToleranceChange
+  end
+  object ButtonOK: TButton
+    Left = 144
+    Top = 112
+    Width = 75
+    Height = 25
+    Caption = 'OK'
+    Default = True
+    TabOrder = 1
+    OnClick = ButtonOKClick
+  end
+  object ButtonCancel: TButton
+    Left = 16
+    Top = 112
+    Width = 75
+    Height = 25
+    Caption = 'Cancel'
+    TabOrder = 2
+    OnClick = ButtonCancelClick
+  end
+  object RadioGroupUnits: TRadioGroup
+    Left = 104
+    Top = 8
+    Width = 104
+    Height = 40
+    Caption = 'Units:'
+    Columns = 2
+    ItemIndex = 0
+    Items.Strings = (
+      'mil'
+      'mm')
+    TabOrder = 3
+    OnClick = RadioGroupUnitsClick
+  end
+  object CheckBoxCopper: TCheckBox
+    Left = 16
+    Top = 72
+    Width = 56
+    Height = 17
+    Caption = 'Copper'
+    Checked = True
+    State = cbChecked
+    TabOrder = 4
+    OnClick = CheckBoxCopperClick
+  end
+  object CheckBoxMech: TCheckBox
+    Left = 88
+    Top = 72
+    Width = 48
+    Height = 17
+    Caption = 'Mech'
+    TabOrder = 5
+    OnClick = CheckBoxMechClick
+  end
+  object CheckBoxCurrent: TCheckBox
+    Left = 152
+    Top = 72
+    Width = 97
+    Height = 17
+    Caption = 'Current'
+    Checked = True
+    State = cbChecked
+    TabOrder = 6
+    OnClick = CheckBoxCurrentClick
+  end
+end
---
+++ PCB/SelectBadConnections/SelectBadConnections02.pas
@@ -0,0 +1,314 @@
+{ SelectBadConnections02.pas   ..............................................
+ Summary
+    Checks if Tracks and Arcs on signal or mech layers connect completely on some
+    other object. Center-to-center check is done.
+    If not connected completely, the objects are displayed with all else masked.
+
+    Signal layer objects MUST have an assigned Net
+    Mech layer objects are NOT checked against Pads Vias or Multi-Layer.
+
+    The tolerance is set in a form. Zero tolerance is supported.
+
+ Created by:    Petar Perisin
+ Modified B.L. Miller
+10/06/2020  1.10  Add support Mech Layers up to eMech24.
+12/06/2020  1.20  fixed problem with iterator layersets & added layer settings to form.
+
+Iterator layer filter methods use different parameters:
+   .AddFilter_IPCB_LayerSet();  requires IPCB_LayerSet & LayerSetUtils interface to work correctly.
+   .AddFilter_LayerSet();       uses TLayerSet, limited to 256 element Delphi/Pascal TSet restriction.
+
+   Can NOT use MkSet, MkRange & SetUnion with IPCB_LayerSet type.
+
+..............................................................................}
+
+{..............................................................................}
+Var
+   Board        : IPCB_Board;
+   Tolerance    : TCoord;
+   Units        : TUnit;
+   bCopper      : boolean;
+   bMech        : boolean;
+   bCurrent     : boolean;
+
+function IsStringANum(Tekst : String) : Boolean;
+var
+   i        : Integer;
+   dotCount : Integer;
+   ChSet    : TSet;
+begin
+   Result := True;
+   // Test for number, dot or comma
+   ChSet := SetUnion(MkSet(Ord('.'),Ord(',')), MkSetRange(Ord('0'), Ord('9')) );
+   for i := 1 to Length(Tekst) do
+      if not InSet(Ord(Tekst[i]), ChSet) then Result := false;
+
+   // Test if we have more than one dot or comma
+   dotCount := 0;
+   ChSet := MkSet(Ord('.'),Ord(','));
+   for i := 1 to Length(Tekst) do
+      if InSet(Ord(Tekst[i]), ChSet) then
+         Inc(dotCount);
+
+   if dotCount > 1 then Result := False;
+end;
+
+function CheckWithTolerance(X1, Y1, X2, Y2) : Boolean;
+begin
+    Result := False;
+    if (Abs(X1 - X2) <= Tolerance) and (Abs(Y1 - Y2) <= Tolerance) then
+        Result := True;
+end;
+
+function CheckObject(Prim1, Prim2 : IPCB_Primitive, X, Y : TCoord) : boolean;
+begin
+    Result := false;
+    case Prim2.ObjectId of
+    eTrackObject :
+        if (Prim2.Layer = Prim1.Layer) then
+            if CheckWithTolerance(Prim2.x1, Prim2.y1, X, Y) or CheckWithTolerance(Prim2.x2, Prim2.y2, X, Y) then
+                 Result := True;
+
+    eArcObject :
+        if (Prim2.Layer = Prim1.Layer) then
+            if CheckWithTolerance(Prim2.StartX, Prim2.StartY, X, Y) or CheckWithTolerance(Prim2.EndX, Prim2.EndY, X, Y) then
+                Result := True;
+
+    ePadObject :
+        if ((Prim2.Layer = eMultiLayer) or (Prim2.Layer = Prim1.Layer)) and CheckWithTolerance(Prim2.x, Prim2.y, X, Y) then
+            Result := True;
+
+    eViaObject :
+        if Prim2.IntersectLayer(Prim1.Layer) and CheckWithTolerance(Prim2.x, Prim2.y, X, Y) then
+            Result := True;
+    end;
+end;
+
+procedure SelectBadConnections(const dummy :boolean);
+var
+   BIter      : IPCB_BoardIterator;
+   SIter      : IPCB_SpatialIterator;
+   Prim1      : IPCB_Primitive;
+   Prim2      : IPCB_Primitive;
+   i          : Integer;
+   X, Y       : TCoord;
+   Found      : Boolean;
+   TempString : String;
+   bMechLayer : boolean;
+   CLayer     : TLayer;
+   LayerSet   : IPCB_LayerSet;
+   dValue     : extended;
+begin
+   TempString := EditTolerance.Text;
+   if LastDelimiter(',.', TempString) = Length(TempString) then
+      SetLength(TempString, Length(TempString - 1))
+   else if LastDelimiter(',.', TempString) <> 0 then
+      TempString[LastDelimiter(',.', TempString)] := DecimalSeparator;
+
+   if Units = eMil then
+   begin
+      Tolerance := MilsToRealCoord(StrToFloat(TempString));
+   end
+   else
+      Tolerance := MMsToRealCoord(StrToFloat(TempString));
+
+   ResetParameters;
+   AddStringParameter('Scope','All');
+   RunProcess('PCB:DeSelect');
+
+   CLayer := Board.CurrentLayer;
+   bMechLayer := false;
+
+   BIter := Board.BoardIterator_Create;
+   BIter.AddFilter_ObjectSet(MkSet(eTrackObject, eArcObject));
+
+   LayerSet := LayerSetUtils.EmptySet;
+   if bCurrent then
+       LayerSet.Include(CLayer);
+   if bCopper then
+       LayerSet.IncludeSignalLayers;
+   if bMech then
+       LayerSet.IncludeMechanicalLayers;
+   if bCopper and bMech then
+       Layerset.IncludeAllLayers;
+
+   BIter.AddFilter_IPCB_LayerSet(LayerSet);
+   Prim1  := BIter.FirstPCBObject;
+   SIter  := Board.SpatialIterator_Create;
+
+   While Prim1 <> nil do
+   begin
+      bMechLayer := LayerUtils.IsMechanicalLayer(Prim1.Layer);
+
+      if Prim1.TearDrop or Prim1.InComponent then
+          Found := true;
+      if (not Prim1.InNet) and not bMechLayer then
+         Found := true
+      else if (Prim1.ObjectId = eArcObject) and (Prim1.StartAngle = 0) and (Prim1.EndAngle = 360) then
+         Found := True
+      else
+
+      LayerSet := LayerSetUtils.EmptySet;
+      LayerSet.Include(Prim1.Layer);
+      if bMechLayer then
+      begin
+          SIter.AddFilter_ObjectSet(MkSet(eTrackObject, eArcObject));
+      end else
+      begin
+          LayerSet.Include(eMultiLayer);
+          SIter.AddFilter_ObjectSet(MkSet(eTrackObject, eArcObject, ePadObject, eViaObject));
+      end;
+      SIter.AddFilter_IPCB_LayerSet(LayerSet);
+
+      for i := 1 to 2 do
+      begin
+         if i = 1 then
+         begin
+            if Prim1.ObjectId = eTrackObject then
+            begin
+               X := Prim1.x1;
+               Y := Prim1.y1;
+            end
+            else
+            begin
+               X := Prim1.StartX;
+               Y := Prim1.StartY;
+            end;
+         end
+         else
+         begin
+            if Prim1.ObjectId = eTrackObject then
+            begin
+               X := Prim1.x2;
+               Y := Prim1.y2;
+            end
+            else
+            begin
+               X := Prim1.EndX;
+               Y := Prim1.EndY;
+            end;
+         end;
+
+         Found := False;
+
+         SIter.AddFilter_Area(X - Tolerance, Y - Tolerance, X + Tolerance, Y + Tolerance);
+         Prim2 := SIter.FirstPCBObject;
+
+         While (Prim2 <> nil) and not Found do
+         begin
+            if (Prim1.I_ObjectAddress <> Prim2.I_ObjectAddress) and not Prim2.TearDrop then
+            begin
+                if (not bMechLayer) and Prim2.InNet then
+                if (Prim2.Net.Name = Prim1.Net.Name) then
+                    Found := CheckObject(Prim1, Prim2, X, Y);
+                if bMechLayer then
+                    Found := CheckObject(Prim1, Prim2, X, Y);
+            end;
+
+            Prim2 := SIter.NextPCBObject;
+         end;
+
+         if not Found then Prim1.Selected := True;
+      end;  // for i
+
+      Prim1 := BIter.NextPCBObject;
+   end;
+   Board.SpatialIterator_Destroy(SIter);
+   Board.BoardIterator_Destroy(BIter);
+
+   Board.SetState_ViewManager_FilterChanging;
+   Client.PostMessage('PCB:RunQuery','Apply=True|Expr=IsSelected|Select=False|Mask=True', Length('Apply=True|Expr=IsSelected|Select=True|Mask=True'), Client.CurrentView);
+// ConfigPCBFilter('Clear=True|Expr=All|Zoom=False|Select=False|Mask=False');
+
+end;
+
+Procedure Start;
+begin
+// form create event has already happened!
+   Board := PCBServer.GetCurrentPCBBoard;
+   if Board = nil then exit;
+   FormSelectBadConnections.ShowModal;
+end;
+
+{..............................................................................}
+procedure TFormSelectBadConnections.ButtonOKClick(Sender: TObject);
+begin
+//    FormSelectBadConnections.Hide;
+    SelectBadConnections(true);
+//    FormSelectBadConnections.Show;
+end;
+
+procedure TFormSelectBadConnections.ButtonCancelClick(Sender: TObject);
+begin
+   FormSelectBadConnections.Close;
+   exit;
+end;
+
+procedure TFormSelectBadConnections.EditToleranceChange(Sender: TObject);
+begin
+   if not IsStringANum(EditTolerance.Text) then
+   begin
+      ButtonOK.Enabled := False;
+      EditTolerance.Font.Color := clRed;
+   end else
+   begin
+      EditTolerance.Font.Color := clWindowText;
+      ButtonOK.Enabled := True;
+   end;
+end;
+
+procedure TFormSelectBadConnections.RadioGroupUnitsClick(Sender: TObject);
+begin
+    Units := eMil;
+    if RadioGroupUnits.ItemIndex = 1 then Units := eMM;
+end;
+
+procedure CheckCurrentCheckBox(const dummy : boolean);
+begin
+    if not (bCopper or bMech) then bCurrent := true;
+    if (bCurrent) then CheckBoxCurrent.State := cbChecked
+    else CheckBoxCurrent.State := cbUnchecked;
+end;
+
+procedure TFormSelectBadConnections.CheckBoxCopperClick(Sender: TObject);
+begin
+    if CheckBoxCopper.State = cbChecked then bCopper := true
+    else bCopper := false;
+    CheckCurrentCheckBox(true);
+end;
+
+procedure TFormSelectBadConnections.CheckBoxMechClick(Sender: TObject);
+begin
+    if CheckBoxMech.State = cbChecked then bMech := true
+    else bMech := false;
+    CheckCurrentCheckBox(true);
+end;
+
+procedure TFormSelectBadConnections.CheckBoxCurrentClick(Sender: TObject);
+begin
+    if CheckBoxCurrent.State = cbChecked then bCurrent := true
+    else bCurrent := false;
+    CheckCurrentCheckBox(true);
+end;
+
+// this event is called immediately script starts NOT when form is shown.
+procedure TFormSelectBadConnections.FormSelectBadConnectionsCreate(Sender: TObject);
+begin
+    bCopper  := true;          // set initial defaults.
+    bMech    := false;
+    bCurrent := true;
+    Units := eMil;
+
+    if (Units = eMil) then RadioGroupUnits.ItemIndex(0)
+    else RadioGroupUnits.ItemIndex(1);
+
+    if (bCopper) then CheckBoxCopper.State := cbChecked
+    else CheckBoxCopper.State := cbUnchecked;
+
+    if (bMech) then CheckBoxMech.State := cbChecked
+    else CheckBoxMech.State := cbUnchecked;
+
+    if (bCurrent) then CheckBoxCurrent.State := cbChecked
+    else CheckBoxCurrent.State := cbUnchecked;
+end;
+
