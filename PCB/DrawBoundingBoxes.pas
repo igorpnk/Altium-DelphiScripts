9f1ae9e1fb18ed673e829ccdadfe5f2620f2e86e
Brett Miller <brett@localhost.localdomain>
2020-09-19 15:03:33 +1200

    Add report with tags & dimensions, make mechlayer visible.

--- PCB/DrawBoundingBoxes.pas
+++ PCB/DrawBoundingBoxes.pas
@@ -9,15 +9,26 @@
 BLM
 05/06/2020  v0.10 POC
 06/06/2020  v0.11 Added text labels & layer enable stuff & support for PcbLib.
+19/09/2020  v0.12 Add report with dimensions. Make the output mech layer visible
 
 Button RunScriptText
 Text=Var B,P,U;Begin    end;
 }
 
 Const
-    iMechLayer = 2;       // Mechanical Layer 2 == 2
+    cShowReport = true;
+    iMechLayer  = 2;       // Mechanical Layer 2 == 2
 
-function DrawBox(Board : IPCB_Board, BR : TCoordRect, UIndex : integer, const Tag : WideString) : boolean;
+var
+    Board     : IPCB_Board;
+    BUnits    : TUnit;
+    BOrigin   : TPoint;
+    Document  : IServerDocument;
+    Filename  : WideString;
+    Report    : TStringList;
+
+
+function DrawBox(BR : TCoordRect, UIndex : integer, const Tag : WideString) : boolean;
 var
     Track    : IPCB_Track;
     Text     : IPCB_Text;
@@ -54,19 +65,23 @@ begin
     Text.YLocation  := BR.y2 - Text.Size;
     Text.UnionIndex := UIndex;
     Board.AddPCBObject(Text);
+    Report.Add(PadRight(Tag, 10) + PadRight(CoordUnitToString(BR.X1-BOrigin.X, BUnits),10) + ' '
+                                 + PadRight(CoordUnitToString(BR.Y1-Borigin.Y, BUnits),10) + ' '
+                                 + PadRight(CoordUnitToString(BR.X2-BOrigin.X, BUnits),10) + ' '
+                                 + PadRight(CoordUnitToString(BR.Y2-BOrigin.Y, BUnits),10) );
 end;
 
 procedure DrawBBoxes;
 var
-    Board   : IPCB_Board;
     PcbLib  : IPCB_Library;
     ML      : TLayer;
     MLayer  : IPCB_MechanicalLayer;
     Prim    : IPCB_Primitive;
-    UIndex  : integer;
-    S       : TStringList;
-    BRect   : TCoordRect;
-    IsLib   : boolean;
+    FPName    : WideString;
+    FPPattern : WideString;
+    UIndex    : integer;
+    BRect     : TCoordRect;
+    IsLib     : boolean;
 
 begin
     Board  := PCBServer.GetCurrentPCBBoard;
@@ -86,41 +101,87 @@ begin
         Prim := PcbLib.CurrentComponent;
     end;
 
+// returns TUnits but with swapped meanings AD17 0 == Metric but API has eMetric=1 & eImperial=0
+    BUnits := Board.DisplayUnit;
+    GetCurrentDocumentUnit;
+    if (BUnits = eImperial) then BUnits := eMetric
+    else BUnits := eImperial;
+
+    BOrigin := Point(Board.XOrigin, Board.YOrigin);
+//    BRBoard := Board.BoardOutline.BoundingRectangle;
+//    Height := RectHeight(BRBoard); // + MilsToCoord(200);
+//    Width  := RectWidth(BRBoard);
+
+    Report := TStringList.Create;
+    Report.Add('Board originX : ' + CoordUnitToString(BOrigin.X, BUnits) + ' originY ' + CoordUnitToString(BOrigin.Y, BUnits));
+//    Report.Add('Board width   : ' + CoordUnitToString(Width, BUnits) +     '  height ' + CoordUnitToString(Height, BUnits));
+    Report.Add('');
+
     ML := LayerUtils.MechanicalLayer(iMechLayer);
     MLayer := Board.LayerStack_V7.LayerObject_V7[ML];
     MLayer.MechanicalLayerEnabled := true;
+    MLayer.IsDisplayed(Board) := true;
     Board.ViewManager_UpdateLayerTabs;
 
     UIndex := GetHashID_ForString(GetWorkSpace.DM_GenerateUniqueID);
 
-// PcbLib component is a group of free primitives on a board.
-//    if not IsLib then
-//        if Prim.ObjectId <> eComponentObject then exit;
-
+    FPName := '';
+    if IsLib then
+    begin
+        FPName    := Prim.Name;
+        FPPattern := '';
+    end else
+    begin
+        if Prim.ObjectId = eComponentObject then
+        begin
+            FPName    := Prim.Name.Text;
+            FPPattern := Prim.Pattern;
+        end;
+    end;
+    FPName := Prim.ObjectIdString + ' ' + FPName + ' ' + FPPattern;
+    Report.Add(FPName);
 
 // Bounding Rectangles
 // PcbDoc: 3 of these are same; NoNameComment Painting Selection
 // PcbLib: 2 are exactly same
 // PcbLib: 2 are almost the same
     BRect := Prim.BoundingRectangle;
-    DrawBox(Board, BRect, UIndex, 'BRect');
+    DrawBox(BRect, UIndex, 'BRect');
     if not IsLib then
     if Prim.ObjectId = eComponentObject then
     begin
 //        BRect := RectToCoordRect(Prim.BoundingRectangleNoNameComment);
         BRect := Prim.BoundingRectangleNoNameComment;
-        DrawBox(Board, BRect, UIndex, 'NNC');
+        DrawBox(BRect, UIndex, 'NNC');
     end;
+
     BRect := Prim.BoundingRectangleChildren;
-    DrawBox(Board, BRect, UIndex, 'Childn');
+    DrawBox(BRect, UIndex, 'Childn');
     BRect := Prim.BoundingRectangleForPainting;
-    DrawBox(Board, BRect, UIndex, 'Paintg');
+    DrawBox(BRect, UIndex, 'Paintg');
     BRect := Prim.BoundingRectangleForSelection;
-    DrawBox(Board, BRect, UIndex, 'Selectn');
+    DrawBox(BRect, UIndex, 'Selectn');
 
 //    Board.CurrentLayer(ML);
     Client.SendMessage('PCB:SetCurrentLayer', 'Layer=' + IntToStr(ML) , 255, Client.CurrentView);
     Board.ViewManager_FullUpdate;
+
+    Report.Insert(0, 'Bounding Boxes Information for ' + ExtractFileName(Board.FileName) + ' document.');
+
+    FileName := ChangeFileExt(Board.FileName,'DBB.txt');
+    Report.SaveToFile(Filename);
+    Report.Free;
+
+    Document  := Client.OpenDocument('Text', FileName);
+    If Document <> Nil Then
+    begin
+        if (cShowReport) then
+        begin
+            Client.ShowDocument(Document);
+            if (Document.GetIsShown <> 0 ) then
+               Document.DoFileLoad;
+        end;
+    end;
 end;
 
 
